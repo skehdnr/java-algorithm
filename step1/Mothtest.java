cf52627efb3ed4ea68a4326de9537afa895cff6b
skehdnr <skehdnr1@naver.com>
2019-08-13 18:16:33 +0900

    first commit

---
+++ step1/Mothtest.java
@@ -0,0 +1,31 @@
+package step1;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Mothtest {
+	public static void main(String[] args) {
+	Date date = new Date();
+	SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd");
+	String now = sdf.format(date);
+	int a = Integer.parseInt(now.substring(6,7));
+	String b = "";
+	
+	switch(a) {
+	case 1 : b = "1월"; break;
+	case 2 : b = "2월"; break;
+	case 3 : b = "3월"; break;
+	case 4 : b = "4월"; break;
+	case 5 : b = "5월"; break;
+	case 6 : b = "6월"; break;
+	case 7 : b = "7월"; break;
+	case 8 : b = "8월"; break;
+	case 9 : b = "9월"; break;
+	case 10 : b = "10월"; break;
+	case 11 : b = "11월"; break;
+	case 12 : b = "12월"; break;
+	default : b = "x"; break;
+	}
+	System.out.println(b);
+	}
+	
+}
---
+++ step1/print5Times.java
@@ -0,0 +1,23 @@
+package step1;
+import java.util.Scanner;
+public class print5Times {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		
+		while(true) {
+			System.out.println("진행1 , 스톱 0");
+			int flag = scanner.nextInt();
+			switch(flag) {
+			case 0 : 
+				System.out.println("시스템 종료");
+				return;
+			case 1 : 
+				System.out.println("시스템 진행");
+				break;
+			}
+			System.out.println("안녕");
+			flag++;
+		}
+	}
+
+}
