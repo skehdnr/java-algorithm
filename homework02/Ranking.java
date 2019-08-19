c1a469be808bdd41402da6b66c09e92ccd6f80ee
skehdnr <skehdnr1@naver.com>
2019-08-19 14:20:27 +0900

    2019-08-19

--- homework02/Ranking.java
+++ homework02/Ranking.java
@@ -6,29 +6,32 @@ public class Ranking {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-	       double[] records = new double[3];
-	       int[] rank = new int[3];
-	       int[] score = new int[3];
-	       String[] player = new String[] {"A","B","C"};
-	       for(int i = 0; i < 3 ; i++) { //순위 배열을 모두 1등으로 초기화 시켜줍니다.
-	          rank[i] = 1;
-	          }
-	       for(int i = 0; i <3 ; i++) { //점수 배열에 학생의 점수를 3번 입력받습니다.
-	          System.out.println("기록을 입력해주세요.");
-	          records[i] = scanner.nextDouble();
-	          System.out.printf("%s의 기록 : %.1f\n",player[i],records[i]);
-	       }
-	       for(int i = 0; i < 3 ; i++) { 
-	          //학생들 끼리의 점수를 비교해서 자신의 점수보다 크면 해당순위를 1씩 증가시켜 순위를 결정.
-	          for(int j = 0 ; j < 3 ; j++) {
-	             if(score[i]< score[j])
-	                rank[i] = rank[i]+1;
-	          }
-	       }
-	       for(int i = 0 ; i < 3 ; i++) { //순위 출력문입니다.
-	          System.out.printf("%d등 : %s 선수기록 (%.1f)\n",i+1,player[i],records[i]);
-	       }
-
+		
+		System.out.printf("A선수 기록을 입력해 주세요\nB선수 기록을 입력해 주세요\nC선수 기록을 입력해 주세요\n");
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+		double c = scanner.nextDouble();
+		int ranking = 0;
+		
+		switch(ranking) {
+		case a : System.out.printf("A선수 기록은 %.1f \n",a);
+		case b : System.out.printf("B선수 기록은 %.1f \n",b);
+		default : System.out.printf("C선수 기록은 %.1f \n",c); break;
+		}
+		System.out.printf("###### 성적 랭킹 ####### \n1등 :%s선수 기록 (%.2f)\n2등 :%s선수 기록 (%.2f)\n3등 :%s선수 기록 (%.2f)\n",a,b,c);
 	}
-
 }
+
+//저희는 비트올림픽 위원회입니다.
+//한번의 트랙에서 육상선수는 총 3명 입니다.
+//총 세명의 100m 기록을 입력받아서 
+//1, 2, 3 등을 가려야 합니다.
+//A 선수 기록 : 10.5초
+//B 선수 기록 : 10.8초
+//C 선수 기록 : 10.1초 
+//라고 입력받았다면
+//###### 성적 랭킹 #######
+//1등 : C 선수 기록 (10.1초)
+//2등 : A 선수 기록 (10.5초)
+//3등 : B 선수 기록 (10.8초)
+//라고 출력시켜주세요.
